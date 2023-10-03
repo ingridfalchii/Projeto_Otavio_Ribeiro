66ef89a34bf6dfd58328cf5da922d35e773b4572
Otávio Henrique Gonçalves Ribeiro <otavio.ribeiro@estudante.ufla.br>
2023-10-03 13:08:19 -0300

    Criação Classe D

---
+++ D.java
@@ -0,0 +1,30 @@
+public class D {
+
+    private int D1;
+    private boolean D2;
+
+    public int getD1() {
+        return D1;
+    }
+
+    public boolean getD2() {
+        return D2;
+    }
+
+    public void setD1(int num) {
+        D1 = num;
+    }
+
+    public void setD2(boolean bool) {
+        D2 = bool;
+    }
+
+    public void MD1() {
+        System.out.println("MD1");
+    }
+
+    public void MD2() {
+        System.out.println("MD2");
+    }
+
+}
