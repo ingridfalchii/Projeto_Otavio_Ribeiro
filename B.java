cd91db1abfdb06217189627d64c50cf1d5e9b71f
Otávio Henrique Gonçalves Ribeiro <otavio.ribeiro@estudante.ufla.br>
2023-10-03 13:07:56 -0300

    Criação método MB3()

--- B.java
+++ B.java
@@ -6,13 +6,15 @@ public class B {
     public int getB1() {
         return B1;
     }
+
     public float getB2() {
         return B2;
     }
-    
+
     public void setB1(int num) {
         B1 = num;
     }
+
     public void setB2(float num) {
         B2 = num;
     }
@@ -20,7 +22,12 @@ public class B {
     public void MB1() {
         System.out.println("MB1");
     }
+
     public void MB2() {
         System.out.println("MB2");
+    }
+
+    public void MB3() {
+        System.out.println("MB3");
     }
 }
