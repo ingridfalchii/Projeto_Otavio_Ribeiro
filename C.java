55d0af463cd09461beb06fc315b390d1c0754c22
Otávio Henrique Gonçalves Ribeiro <otavio.ribeiro@estudante.ufla.br>
2023-10-03 12:52:35 -0300

    Criação da Classe C

---
+++ C.java
@@ -0,0 +1,29 @@
+public class C {
+
+    private String C1;
+    private int C2;
+
+    public String getC1() {
+        return C1;
+    }
+
+    public int getC2() {
+        return C2;
+    }
+
+    public void setC1(String txt) {
+        C1 = txt;
+    }
+
+    public void setC2(int num) {
+        C2 = num;
+    }
+
+    public void MC1() {
+        System.out.println("MC1");
+    }
+
+    public void MC2() {
+        System.out.println("MC2");
+    }
+}
